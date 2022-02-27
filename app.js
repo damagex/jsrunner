778de2d179e48b3a2c26cd869fb4fd313e710063
damagex <davem1991@gmail.com>
2022-02-27 23:34:39 +0100

    .

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
 const router = express.Router();
 const app = express();
-const cors = require("cors");
-const _eval = require("eval");
-
 let logs = []
-let changeConsole = ["log", "warn", "error"];
+let prev = "";
+let changeConsole = ["log", "info", "warn", "error", "fatal", "timeEnd", "timeLog", "timeStamp", "table", "dir", "dirxml", "system"];
 const defConsole = {};
 const switchConsole = () => {
-    logs = [];
-    changeConsole.forEach((type) => {
+    changeConsole.forEach(type => {
         defConsole[type] = console[type];
         console[type] = function () {
             let args = Array.from(arguments);
-            logs.push({
-                type: type,
-                args: args
-            });
+            if (!type.includes("time") && !type.includes("table")) {
+                if (prev === "time" && type === "log") {
+                    args[0] = "Timer: " + args[0]
+                    args.shift();
+                }
+                logs.push({
+                    type: type,
+                    args: args
+                });
+            }
+            prev = type;
+            type = type === "fatal" ? "error" : type;
             defConsole[type].apply(console, args);
         }
+    })
+}
+
+const revertConsole = () => {
+    changeConsole.forEach(con => {
+        console[con] = defConsole[con];
     })
 }
 
@@ -28,23 +41,30 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const exec = (run) => (`
-    module.exports = () => {
-        let logs = [];
-        let changeConsole = ["log", "warn", "error"];
-        switchConsole();
-        try {
-            ${run}
-        } catch (err) {
-            console.error(err)
-        }
-        return logs;
+    let logs = [];
+    let prev = "";
+    let changeConsole = [${changeConsole.map(i => "'" + i + "'")}];
+    const defConsole = {};
+    const switchConsole = ${switchConsole};
+    const revertConsole = ${revertConsole};
+    switchConsole();
+    try {
+        ${run}
+    } catch (err) {
+        console.error(JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err))))
     }
+    revertConsole();
+    resolve(logs);
 `);
 
-const handleRunner = (req, res) => {
-    const result = _eval(exec(req.body.run), "jsrunner", {
-        switchConsole: switchConsole
+const waitEval = (ev) => {
+    return new Promise((resolve, reject) => {
+        new Function('resolve', ev)(resolve);
     });
+};
+
+const handleRunner = async (req, res) => {
+    const result = await waitEval(exec(req.body.run));
     console.log(result);
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(result));
