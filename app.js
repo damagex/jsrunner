5cda44ec89eb50507cf61700bf60eb7e62d8cd01
damagex <davem1991@gmail.com>
2022-02-27 19:59:28 +0100

    .

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 const app = express();
 const cors = require("cors");
-
+const fs = require("fs");
+const childProcess = require('child_process');
 
 let logs = []
 let changeConsole = ["log", "warn", "error"];
@@ -30,38 +31,56 @@ const revertConsole = () => {
 }
 
 app.use(cors())
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.post("/run", (req, res) => {
-    console.log("starting");
-    switchConsole();
-    try {
-        eval(req.body.run);
-    } catch (err) {
-        console.error(err)
-    }
-    revertConsole();
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(logs));
-});
+const createScript = (content, callback) => {
+    const filename = "run-" + new Date().getTime() + ".js";
+    fs.writeFile(filename, content, (err) => {
+        if (err) throw err;
+        console.log('File is created successfully.');
+    });
+    callback(filename);
+}
 
-// add router in the Express app.
+const runScript = (scriptPath, callback) => {
+    let invoked = false;
+    let process = childProcess.fork(scriptPath);
+
+    process.on("error", function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err, process);
+    });
+
+    process.on("exit", function (code) {
+        if (invoked) return;
+        invoked = true;
+        const err = code === 0 ? null : new Error("exit code " + code);
+        callback(err, process);
+    });
+}
+
+const handleRunner = (req, res) => {
+    createScript(req.body.run, filename => {
+        runScript('./' + filename, (err, process) => {
+            if (err) throw err;
+            logs.push({
+                type: "log",
+                args: process.stdout
+            })
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(logs));
+        });
+    });
+}
+
+router.post("/run", handleRunner);
 app.use("/", router);
 
 const port = process.env.RUNNER_PORT || 8080;
 const host = process.env.RUNNER_HOST || "0.0.0.0";
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 app.listen(port, host, () => {
     console.log("Started on " + host + ":" + port);
 })
-
-/*
-curl --request POST \
-  --url http://92.222.231.56:27030/run \
-  --header 'content-type: application/json' \
-  --data '{
-	"msg": "testing"
-}'
- */
